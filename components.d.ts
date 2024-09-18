76ed584e3487ba2b99d70373564bb47b820e59ca
黄世庚 <shigengvi@163.com>
2024-09-18 16:17:35 +0800

    文档：更新配置项

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,8 @@ declare module 'vue' {
     ComponentB: typeof import('./src/components/ComponentB.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
